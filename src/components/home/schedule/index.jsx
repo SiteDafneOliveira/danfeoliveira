666b1d830fe7e2eb5b743edc4eadadc9927018cd
Debora <site.dafneoliveira@gmail.com>
2025-04-27 12:36:42 -0700

    chore: update schedule

--- src/components/home/schedule/index.jsx
+++ src/components/home/schedule/index.jsx
@@ -1,10 +1,15 @@
 import homeS from "@/styles/Home.module.css";
 import { WHATS_LINK } from "@/utils/links";
 import s from "./schedule.module.scss";
+import { useRef } from "react";
+import useOnScreen from "@/utils/useOnScreen";
 
 const Schedule = () => {
+  const ref = useRef(null);
+  const isVisible = useOnScreen(ref);
+
   return (
-    <div className={s.schedule} id='agendar'>
+    <div ref={ref} className={`${s.schedule} ${isVisible ? s.visible : ""}`} id='agendar'>
       <div className={`container ${s.container}`}>
         <h2 className={s.title}>
           Agende sua consulta e viva o resultado de uma nutrição realmente
